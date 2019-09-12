c595ed3991c27b25111061c16fb6a3a0e59c532c
João Graça <joao.g.n91@gmail.com>
2019-09-12 00:14:33 -0300

    create Profile features

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const dispach = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispach(updateProfileRequest(data));
+  }
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+        <hr />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Sua nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button" onClick={() => dispach(signOut())}>
+        Sair do GoBarber
+      </button>
+    </Container>
+  );
 }
